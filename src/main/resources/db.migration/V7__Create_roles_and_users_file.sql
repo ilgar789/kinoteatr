8293e26f68b2e7fddf55532aa98e76b3d8d070e6
ilgar <ilgar.aghamov@gmail.com>
2023-04-04 13:42:20 +0400

    Bugs fixed

--- src/main/resources/db.migration/V7__Create_roles_and_users_file.sql
+++ src/main/resources/db.migration/V7__Create_roles_and_users_file.sql
@@ -1,4 +1,5 @@
 INSERT INTO public.t_role(id, name)
 VALUES (1, 'ROLE_USER'), (2, 'ROLE_ADMIN');
+INSERT INTO t_user VALUES (1,'user','user',current_timestamp );
 INSERT INTO public.t_user_roles(user_id, roles_id)
 VALUES (1, 2);
